ab4c254511fa4a43ed9d4e923ffc13a8e494692a
DoanAllie <doan.allie@gmail.com>
2022-09-12 20:34:08 -0500

    ready for deploment

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../index.html '))
+})
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`vibing on port ${port}`)
+})
