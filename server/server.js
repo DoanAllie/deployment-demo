a8470c9d5881100583c7e3476f2e879358fa55fc
DoanAllie <doan.allie@gmail.com>
2022-09-12 21:23:17 -0500

    deploy again

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html '))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
-    console.log(`vibing on port ${port}`)
+    console.log(`Vibing on port ${port}`)
 })
